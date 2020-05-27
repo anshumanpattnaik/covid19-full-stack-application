5604efb5ed3bcd7ae093feb2867866a1ec7774ee
Ravindra jitpure <ravindra.jitpure@mountblue.tech>
2020-05-27 16:14:05 +0530

    Removed the Baseurl and Maptoken

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -5,8 +5,7 @@ export const MAP_STYLE= 'MAP_STYLE';
 export const SET_ACTION = 'SET_ACTION';
 export const MAP_STYLE_ACTION = 'MAP_STYLE_ACTION';
 export const MAP_FLY_ACTION = 'MAP_FLY_ACTION';
-export const BASE_URL = `https://intense-castle-52721.herokuapp.com/`;
-export const MAPBOX_ACCESS_TOKEN = `pk.eyJ1IjoicmF2aW5kcmFqaXRwdXJlIiwiYSI6ImNrYW1qMnQweTE0a3QzMHBpN3V0aG93Z2gifQ._e8p1pniJAvzXLPMuazBaw`;
 
-// export const BASE_URL = `http://YOUR_LOCAL_IP_ADDRESS:9000`;
-// export const MAPBOX_ACCESS_TOKEN = `YOUR_MAPBOX_API_TOKEN`;
+
+export const BASE_URL = `http://YOUR_LOCAL_IP_ADDRESS:9000`;
+export const MAPBOX_ACCESS_TOKEN = `YOUR_MAPBOX_API_TOKEN`;
